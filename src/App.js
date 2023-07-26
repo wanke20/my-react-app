b4f771c4ce862b5a83598e54fb2711448f90a482
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-07-26 02:10:20 -0400

    delete comments

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { HomePage, CounterButtonPage, PeopleListPage } from "./pages";
 
 function App() {
-  // not needed for now
   // const [numberOfClicks, setNumberOfClicks] = useState(0);
   // const [hideMessage, setHideMessage] = useState(false);
 
