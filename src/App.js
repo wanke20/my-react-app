dc35414dfe524d113eb98476e9be1baae93508c4
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-07-25 20:59:30 -0400

    improve modularity

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { Greeting } from "./Greeting";
+import { PeopleList } from "./PeopleList";
+import React from "react";
+
+const people = [{
+  name: 'John',
+  age: 40,
+  hairColor: 'brown',
+}, {
+  name: 'Helga',
+  age: 25,
+  hairColor: 'red',
+}, {
+  name: 'Dwayne',
+  age: 55,
+  hairColor: 'blonde',
+}]
 
 function App() {
+  let adjective = "cool";
+  let url = "https://reactjs.org";
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
---
+++ src/PeopleList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PeopleListItem } from './PeopleListItem';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+    border: 2px solid white;
+    padding: 32px;
+`;
+
+export const PeopleList = ({ people }) => (
+    <Wrapper>
+        {people.map(person => <PeopleListItem person={person}
+            key={person.name} />)}
+    </Wrapper>
+);
